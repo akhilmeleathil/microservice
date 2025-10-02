4b2d346f729fce168ddf6064189e315e1ccf1f76
Akhil <sonumahesh113@gmail.com>
2025-10-02 23:13:38 +0530

    Simple App creation.

---
+++ simpleApp/src/main/java/com/learning/simpleProductApp/SimpleProductAppApplication.java
@@ -0,0 +1,13 @@
+package com.learning.simpleProductApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProductAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProductAppApplication.class, args);
+	}
+
+}
