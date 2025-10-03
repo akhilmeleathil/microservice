fe4d964fd99e092e36d72b1c009a8d0b91d5ea3a
akhilmeleathil <103417833+akhilmeleathil@users.noreply.github.com>
2025-10-03 09:51:15 +0530

    Delete simpleApp directory

--- simpleApp/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO product (prod_name, prod_price)
-VALUES 
-  ('Protein Powder', 1200),
-  ('Yoga Mat', 750),
-  ('Dumbbells Set', 2500),
-  ('Resistance Bands', 500),
-  ('Fitness Tracker', 3200);
